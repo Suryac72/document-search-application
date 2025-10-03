03da98f1f6ee0cb593dadd68c7d503a670de6401
Surya Prakash Chaudhary <suryac72@gmail.com>
2025-10-03 18:43:30 +0530

    feat:codebase setup for document-search-application

---
+++ backend/src/test/java/com/forrester/backend/DocumentSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forrester.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentSearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
